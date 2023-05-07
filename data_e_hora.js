23c66d5d1d0643ed7247900790e4576b56d4b050
Stanley Andrade <45856878+StanleyAndrade@users.noreply.github.com>
2023-05-06 22:15:35 -0300

    Update data_e_hora.js

--- data_e_hora.js
+++ data_e_hora.js
@@ -1,5 +1,4 @@
 //Pega dia e hora e coloca na variável dataAtual
-const data = new Date()
 const dia = String(data.getDate()).padStart(2, '0') //trasforma 1 em 01
 const mes = String(data.getMonth() + 1).padStart(2, '0') //trasforma 1 em 01
 const ano = data.getFullYear() // 2023
@@ -8,3 +7,8 @@ const minuto = String(data.getMinutes()).padStart(2, '0') //trasforma 1 em 01
 const segundo = String(data.getSeconds()).padStart(2, '0') //trasforma 1 em 01
 const dataAtual = `Enviado em ${dia}/${mes}/${ano} às ${hora}:${minuto}:${segundo}`
 ${dataAtual} 
+
+//Descobre qual é o dia da semana 
+//Usa a var "data", que já foi declarada lá em cima
+var semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+console.log('Dia é ' + semana[data.getDay()]);
